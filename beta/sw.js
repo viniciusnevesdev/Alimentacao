087a29b373208005695eb76c0d6bbc40e993c42e
Vinicius <viniciusnevez123@gmail.com>
2026-09-10 05:04:30 -0300

    Adiciona cache independente da beta

---
+++ beta/sw.js
@@ -0,0 +1,8 @@
+const CACHE='nutritrack-beta-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./beta-app.js','../index.html','../styles.css','../nutrition-data.js','../app.js'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('nutritrack-beta-')&&key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+});
