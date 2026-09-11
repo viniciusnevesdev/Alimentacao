5abf8c8a48192dd7710b80503f7301eb0fd2968f
Vinicius <viniciusnevez123@gmail.com>
2026-09-11 04:59:47 -0300

    Refresh beta cache after head style fix

--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-const CACHE='nutritrack-beta-v12';
+const CACHE='nutritrack-beta-v13';
 const ASSETS=['./','./index.html','../layout-fixes.css','./manifest.webmanifest','./app.js','./base-index.html','./base-app.js','./styles.css','./nutrition-data.js','./tab-animation.css','./tab-animation.js','./icon-beta-192.png','./icon-beta-512.png','./apple-touch-icon.png','./favicon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();}); 
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('nutritrack-beta-')&&key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
