0b610aade44cd6f43596338e8cc370955a405ca5
Vinicius <viniciusnevez123@gmail.com>
2026-09-11 05:20:37 -0300

    Refresh icon lab cache after SVG parser update

--- icon-studio/sw.js
+++ icon-studio/sw.js
@@ -1,4 +1,4 @@
-const CACHE="alimentacao-icon-lab-v4";
+const CACHE="alimentacao-icon-lab-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","../icon-menu-192.png?v=2","../icon-menu-512.png?v=2"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith("alimentacao-icon-lab-")&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
